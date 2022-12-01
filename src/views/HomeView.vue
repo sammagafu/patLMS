74b40ab17fc57fa612bd8d09583604ffcfec5317
Samuel Magafu <hi@sammagafu.com>
2022-12-01 16:55:38 +0300

    fixed the default template

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,68 +1,24 @@
 <template>
-    <div class="py-16">
+    <div class="pt-16">
         <h2 class="py-4 font-bold dark:text-gray-400 text-3xl">Welcome {{userstore.userData.full_name}}</h2>
-        <div class="grid grid-cols-1 lg:grid-cols-3 gap-8 dark:text-gray-100">
-
-            <div class="card rounded-lg shadow py-9 px-8 dark:bg-slate-900">
-                <div class="flex flex-row items-center justify-between">
-                    <div class="flex flex-col items-start">
-                        <div class="text-baseline">Available courses</div>
-                        <div class="text-5xl font-bold pt-2">10,800</div>
-                    </div>
-
-                    <div class="">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
-                        </svg>
-
-                    </div>
-                </div>
-            </div>
-
-            <div class="card rounded-lg shadow py-9 px-8 dark:bg-slate-900">
-                <div class="flex flex-row items-center justify-between">
-                    <div class="flex flex-col items-start">
-                        <div class="text-baseline">Courses</div>
-                        <div class="text-5xl font-bold pt-2">10,800</div>
-                    </div>
-
-                    <div class="">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5" />
-                        </svg>
-
-                    </div>
-                </div>
-            </div>
-
-            <div class="card rounded-lg shadow py-9 px-8 dark:bg-slate-900">
-                <div class="flex flex-row items-center justify-between">
-                    <div class="flex flex-col items-start">
-                        <div class="text-baseline">Instructors</div>
-                        <div class="text-5xl font-bold pt-2">10,800</div>
-                    </div>
-
-                    <div class="">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
-                        </svg>
-                    </div>
-                </div>
-            </div>
-
-
-        </div>
     </div>
+    <div v-if="userstore.userData.is_student">
+        <UserDashboard></UserDashboard>
+    </div>
+    <div v-else-if="userstore.userData.is_tuitor">
+
+    </div>
+
 </template>
 
 <script>
+// import Modal from "@/components/Modal.vue";
+// import UserDashboard from "@components/UserDashboard.vue";
+import AdminDashboard from "@/components/AdminDashboard.vue";
+import UserDashboard from "../components/UserDashboard.vue";
+
 import { userStore, pagesInteractivity } from '@/stores/index'
+
 
 export default{
     setup(){
@@ -71,6 +27,9 @@ export default{
         return {
             userstore
         }
+    },
+    components : {
+        AdminDashboard,UserDashboard
     }
 }
 </script>
